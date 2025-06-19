6ff84a8341a5c862b0f3140ade2e010b5c55b6ff
JaViKo500 <javikogutierrez64@gmail.com>
2025-06-18 22:10:26 -0500

    fix: update validate code province to foreign

--- lib/helpers/identification/validate_code_province.dart
+++ lib/helpers/identification/validate_code_province.dart
@@ -5,10 +5,13 @@ void validateCodeProvince ( String codeProvince ) {
   const int minProvinceCode = 0;
   const int maxProvinceCode = 24;
   final code = int.tryParse(codeProvince);
-  if (code == null ||code < minProvinceCode || code > maxProvinceCode) {
+  
+  if ( code == 30 ) return;
+
+  if ( code == null ||code < minProvinceCode || code > maxProvinceCode) {
     throw IdentificationException(
       ErrorCode.invalidCodeProvince,
-      'Invalid province code: the first two digits must be a number between 00 and 24.',
+      'Invalid province code: the first two digits must be a number between 00 - 24 and 30 foreign.',
     );
   }
 }
